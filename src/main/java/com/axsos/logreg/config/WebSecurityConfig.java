5d820adee52c3279c57b403d7017e3c30ba51e2b
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-11-22 08:48:03 +0200

    security initial

--- src/main/java/com/axsos/logreg/config/WebSecurityConfig.java
+++ src/main/java/com/axsos/logreg/config/WebSecurityConfig.java
@@ -1,26 +1,28 @@
-//package  com.axsos.logreg.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//@Configuration
-//public class WebSecurityConfig {
-//	@Bean
-//	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//		
-//		http.
-//	            authorizeRequests()
-//	                .antMatchers("/css/**", "/js/**", "/registration").permitAll()
-//	                .anyRequest().authenticated("/admin")
+package  com.axsos.logreg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class WebSecurityConfig {
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		http.
+	            authorizeRequests()
+	                .antMatchers("/admin/**").hasRole("ADMIN") 
+//	                .antMatchers("/css/**", "/js/**","/","/Regpage","/loginpage").permitAll()
+	                .anyRequest().permitAll();
 //	                .and()
-//	            .formLogin()
-//	                .loginPage("/login")
+//	                .antMatchers("/admin").hasRole("ADMIN");
+//	            	.formLogin()
+//	                .loginPage("/loginpage");
 //	                .permitAll()
 //	                .and()
-//	            .logout()
+//	                .logout()
 //	                .permitAll();
-//		
-//		return http.build();
-//	}
-//}
+		
+		return http.build();
+	}
+}
